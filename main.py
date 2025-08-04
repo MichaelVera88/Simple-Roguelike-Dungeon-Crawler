fdd7b325d804813fd07ed734376f00cf34dc9604
Michael Vera <michaelgvera88@gmail.com>
2025-08-04 19:30:38 -0400

    Adding Comments to finish developing FR-001: Setup Game Window

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
 
+# Initialize & Set Game Window
 pygame.init()
 window = pygame.display.set_mode((600,500))
-running = True
 
 window_icon = pygame.image.load("Static/Images/window_icon_sword.png").convert_alpha()
 
 pygame.display.set_icon(window_icon)
 pygame.display.set_caption("Roguelike Dungeon Crawler")
 
+# Run Game Window Loop
+running = True
 while running:
 
     for event in pygame.event.get():
