fd4ae2ee38e61edf78eb4b361203d28a21e9ead3
Michael Vera <michaelgvera88@gmail.com>
2025-08-04 17:48:38 -0400

    Changed window resolution

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode((500,800))
+window = pygame.display.set_mode((600,500))
 window.fill("purple")
 pygame.display.flip()
 time.sleep(3)
