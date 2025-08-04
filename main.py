d500ba1af82100dd07cea6c784455b3d6e851405
Michael Vera <michaelgvera88@gmail.com>
2025-08-04 17:56:34 -0400

    Added window loop

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import time
 import pygame
 
 pygame.init()
 window = pygame.display.set_mode((600,500))
-pygame.display.flip()
-time.sleep(3)
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+    
+    window.fill("red")
+    pygame.display.flip()
+    pygame.time.Clock().tick(30)
+
 pygame.quit()
