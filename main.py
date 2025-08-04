4f70c5546a51dce076dfa5960bc91fd75a38aabc
Michael Vera <michaelgvera88@gmail.com>
2025-08-04 19:47:32 -0400

    Testing blit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pygame.init()
 window = pygame.display.set_mode((600,500))
 
 window_icon = pygame.image.load("Static/Images/window_icon_sword.png").convert_alpha()
+game_title = pygame.image.load("Static/Images/black_rect.png").convert_alpha()
 
 pygame.display.set_icon(window_icon)
 pygame.display.set_caption("Roguelike Dungeon Crawler")
@@ -17,8 +18,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    window.fill("red")
-    window.blit(window_icon, (0,0))
+    window.fill("black")
+    window.blit(game_title, (0,0))
     pygame.display.flip()
     pygame.time.Clock().tick(30)
 
