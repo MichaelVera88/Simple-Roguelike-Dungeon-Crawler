d5ef5143a0c3335737238af4b6aa650ed8cb5156
Michael Vera <michaelgvera88@gmail.com>
2025-08-04 19:16:10 -0400

    Editing game window

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ pygame.init()
 window = pygame.display.set_mode((600,500))
 running = True
 
+window_icon = pygame.image.load("\Static\Images\window_icon_sword.png").convert_alpha()
+
 while running:
 
     for event in pygame.event.get():
@@ -11,6 +13,8 @@ while running:
             running = False
     
     window.fill("red")
+    pygame.display.set_icon(window_icon)
+    pygame.display.get_caption("Roguelike Dungeon Crawler")
     pygame.display.flip()
     pygame.time.Clock().tick(30)
 
