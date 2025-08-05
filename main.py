b68e56d3af7bb9ac1f699a7d9a9f40da21e58915
Michael Vera <michaelgvera88@gmail.com>
2025-08-04 23:55:51 -0400

    Added reactive play button

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ window = pygame.display.set_mode((600,500))
 window_icon = pygame.image.load("Static/Images/window_icon_sword.png").convert_alpha()
 game_title = pygame.image.load("Static/Images/game_title.png").convert_alpha()
 
+play_button = pygame.image.load("Static/Images/UI/play_button.png").convert_alpha()
+play_button_hover = pygame.image.load("Static/Images/UI/play_button_hover.png").convert_alpha()
+play_button_hitbox = play_button.get_rect(topleft=(100,100))
+
 pygame.display.set_icon(window_icon)
 pygame.display.set_caption("Roguelike Dungeon Crawler")
 
@@ -18,6 +22,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    mouse_pos = pygame.mouse.get_pos()
+
+    if play_button_hitbox.collidepoint(mouse_pos):
+        window.blit(play_button_hover, play_button_hitbox)
+    else:
+        window.blit(play_button, play_button_hitbox)
+
     window.fill("black")
     window.blit(game_title, (100,50))
     pygame.display.flip()
