617d1203788ccb690815a558c98aa56ea1cfdd71
Michael Vera <michaelgvera88@gmail.com>
2025-08-05 00:00:04 -0400

    Editing reactive play button

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ while running:
     
     mouse_pos = pygame.mouse.get_pos()
 
-    window.blit(play_button, play_button_hitbox)
+    if play_button_hitbox.collidepoint(mouse_pos):
+        window.blit(play_button_hover, play_button_hitbox)
+    else:
+        window.blit(play_button, play_button_hitbox)
 
     window.blit(game_title, (100,50))
     pygame.display.flip()
