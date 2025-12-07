b6e09800eb41e518adfbf64f95808c6d33282995
Michael Vera <michaelgvera88@gmail.com>
2025-12-06 20:27:59 -0500

    Added hover UI elements to generated rooms

--- src/scenes/dungeon_overview.py
+++ src/scenes/dungeon_overview.py
@@ -12,11 +12,13 @@ class DungeonOverview(base.BaseScene):
         # Load Static Files
         player_marker = self.static_folder / "Images" / "UI" / "Dungeon" / "player_marker.png"
         dungeon_room = self.static_folder / "Images" / "UI" / "Dungeon" / "dungeon_room.png"
+        dungeon_room_hover = self.static_folder / "Images" / "UI" / "Dungeon" / "dungeon_room_hover.png"
 
         # Static Files to Pygame
         self.player_marker = pygame.image.load(player_marker).convert_alpha()
         self.start_dungeon_room = pygame.image.load(dungeon_room).convert_alpha()
         self.dungeon_room = pygame.image.load(dungeon_room).convert_alpha()
+        self.dungeon_room_hover = pygame.image.load(dungeon_room_hover).convert_alpha()
 
         # Initialize Dungeon Generation
         self.dg = dungeon_generation.DungeonGeneration()
@@ -25,11 +27,16 @@ class DungeonOverview(base.BaseScene):
     def handle_events(self, events):
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                for child in self.current_room.children:
-                    if child is None:
-                        print("None")
-                    else:
-                        print("Room Generated")
+                for index, room in enumerate(self.current_room.children):
+                    dungeon_room_hitbox = self.dungeon_room.get_rect(topleft=room.room_pos)
+                    if dungeon_room_hitbox.collidepoint(pygame.mouse.get_pos()):
+                        match index:
+                            case 0:
+                                print("Left Room Selected")
+                            case 1:
+                                print("Forward Room Selected")
+                            case 2:
+                                print("Right Room Selected")
 
     def handle_updates(self):
         pass 
@@ -38,7 +45,15 @@ class DungeonOverview(base.BaseScene):
         # Clear Window
         window.fill((0,0,0))
 
+        # Starting Dungeon Room
         window.blit(self.start_dungeon_room, self.dg.dungeon_start.room_pos)
+
+        # Display connecting rooms from current room
         for room in self.current_room.children:
-            window.blit(self.dungeon_room, room.room_pos)
+            dungeon_room_hitbox = self.dungeon_room.get_rect(topleft=room.room_pos)
+            if dungeon_room_hitbox.collidepoint(pygame.mouse.get_pos()):
+                window.blit(self.dungeon_room_hover, dungeon_room_hitbox)
+            else:
+                window.blit(self.dungeon_room, room.room_pos)
+
         window.blit(self.player_marker, (288,438))
