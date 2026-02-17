183b1e30f41f7aabd79ed6e245052751da412238
Michael Vera <michaelgvera88@gmail.com>
2026-02-17 18:02:32 -0500

    Added more details to combat rooms

--- src/logic/player.py
+++ src/logic/player.py
@@ -9,7 +9,7 @@ class Player(base_entity.Entity):
     def __init__(self):
         super().__init__()
 
-        self.health_potions = 0
+        self.health_potions = 3
     
     def attack(self):
         pass
@@ -17,3 +17,5 @@ class Player(base_entity.Entity):
     def use_health_potion(self):
         self.health += 5
         self.health_potions -= 1
+        if self.health > 10:
+            self.health = 10
--- src/scenes/dungeon_combat_room.py
+++ src/scenes/dungeon_combat_room.py
@@ -13,14 +13,19 @@ class DungeonCombatRoom(base.BaseScene):
 
         # Load Static Files
         enemy_skeleton = self.static_folder / "Images" / "UI" / "Enemies" / "skeleton.png"
+        health_potion = self.static_folder / "Images" / "UI" / "Items" / "health_potion.png"
         
         # Static Files to Pygame
         self.enemy = pygame.image.load(enemy_skeleton).convert_alpha()
         self.enemy_hitbox = self.enemy.get_rect(topleft=(200,300))
 
+        self.health_potion_button = pygame.image.load(health_potion).convert_alpha()
+        self.health_potion_button_hitbox = self.health_potion_button.get_rect(topleft=(100,100))
+
         # Load Player
         self.player = player
         self.turns = 1
+        self.click_health_potion_button = False
 
         # Load Enemy Entities
         self.skeleton = enemy.Enemy()
@@ -31,6 +36,8 @@ class DungeonCombatRoom(base.BaseScene):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.enemy_hitbox.collidepoint(pygame.mouse.get_pos()):
                     self.click_skeleton = True
+                if self.health_potion_button_hitbox.collidepoint(pygame.mouse.get_pos()):
+                    self.click_health_potion_button = True
 
     def handle_updates(self):
         if self.click_skeleton:
@@ -47,6 +54,11 @@ class DungeonCombatRoom(base.BaseScene):
                 self.player.health -= damage
                 self.turns = 2
         
+        if self.click_health_potion_button:
+            if self.player.health_potions > 0:
+                self.player.use_health_potion()
+            self.click_health_potion_button = False
+
         if self.player.health <= 0:
             self.next_scene = "main_menu"
 
@@ -62,3 +74,8 @@ class DungeonCombatRoom(base.BaseScene):
 
         player_health = font.render(f"{self.player.health}", True, (255, 255, 255))
         window.blit(player_health, (0,0))
+
+        window.blit(self.health_potion_button, self.health_potion_button_hitbox)
+
+        health_potion_count = font.render(f"{self.player.health_potions}", True, (0, 0, 0))
+        window.blit(health_potion_count, self.health_potion_button_hitbox)
